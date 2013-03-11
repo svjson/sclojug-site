e73066ff24e947d86341a137b333cdbc9412a6a8
Ville Svärd <ville.svard@agical.com>
2013-03-11 19:43:47 +0100

    Implementing echo service

--- src/sclojug_site/handler.clj
+++ src/sclojug_site/handler.clj
@@ -2,16 +2,26 @@
   (:use compojure.core                
         ring.middleware.resource
         ring.middleware.file-info
+        ring.middleware.edn
         hiccup.middleware)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [sclojug-site.common :as common]))
 
-(defn home [] 
+(defn home []
   (common/layout [:h1 "Hello World!"]))
+
+(defn echo [name params]
+  {:status 200
+   :headers {"Content-Type" "application/edn"}
+   :body (pr-str {:name name
+                  :params params})})
 
 (defroutes app-routes
   (GET "/" [] (home))
+  (GET "/echo/:name"
+       [name :as {params :params}]
+       (echo name params))
   (route/resources "/")
   (route/not-found "Not Found"))
 
@@ -19,8 +29,9 @@
 
 (def war-handler 
   (-> app    
-    (wrap-resource "public") 
-    (wrap-base-url)
-    (wrap-file-info)))
-  
+      (wrap-resource "public")
+      (wrap-edn-params)
+      (wrap-base-url)
+      (wrap-file-info)))
+
 
