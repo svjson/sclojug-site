dd47ca83ef237fda7c9fb0e25be96def7302b6ce
Ville Svärd <ville.svard@agical.com>
2013-03-11 19:43:35 +0100

    Setting clojure version to 1.5.0 and adding ring-edn

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject sclojug-site "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [compojure "1.1.3"]
                  [hiccup "1.0.2"]
                  [ring/ring-jetty-adapter "1.1.0"]
+                 [fogus/ring-edn "0.2.0-SNAPSHOT"]
                  [bultitude "0.1.7"]]
   :plugins [[lein-ring "0.8.3"]]
   :ring {:handler sclojug-site.handler/war-handler}
